b9f7d9f1b553a93cfd18024f2d40debb7695e3cc
Fabian <fab.hof@gmx.de>
2019-12-19 17:29:26 +0100

    add chunksize argument

--- netallocation/__init__.py
+++ netallocation/__init__.py
@@ -10,6 +10,7 @@ from .flow import flow_allocation as allocate_flow
 from .grid import (Incidence, network_flow, network_injection,
                    power_demand, power_production)
 from .linalg import diag, inv, pinv
+from .io import load_dataset, store_dataset
 
 from . import (utils, breakdown, grid, flow, linalg, plot, cost,
                plot_helpers, test, io, common, process)
--- netallocation/breakdown.py
+++ netallocation/breakdown.py
@@ -8,14 +8,14 @@ Created on Thu Mar  7 15:38:24 2019
 
 from .grid import power_demand, power_production
 from .utils import as_sparse, obj_if_acc
+from sparse import COO
 import logging
 from dask.diagnostics import ProgressBar
 
 logger = logging.getLogger(__name__)
 
 def expand_by_source_type(ds, n, components=['Generator', 'StorageUnit'],
-                          dim='source', cut_lower_share=1e-5, sparse=True,
-                          chunk=None):
+                          chunksize=None):
     """
     Breakdown allocation into generation carrier type.
 
@@ -31,7 +31,9 @@ def expand_by_source_type(ds, n, components=['Generator', 'StorageUnit'],
     components : list, default ['Generator', 'StorageUnit']
         List of considered components. Carrier types of these components are
         taken for breakdown.
-
+    chunksize : int
+        Chunksize of the snapshot chunks passed to dask for computing faster
+        and with less memory usage for large datasets.
 
     Example
     -------
@@ -43,12 +45,16 @@ def expand_by_source_type(ds, n, components=['Generator', 'StorageUnit'],
     sns = ds.get_index('snapshot')
     share = (power_production(n, sns, per_carrier=True) / power_production(n, sns))
     share = share.rename(bus='source', carrier='source_carrier')
-    if sparse:
+    if all(isinstance(ds[v].data, COO) for v in ds):
         share = as_sparse(share.fillna(0))
+    elif any(isinstance(ds[v], COO) for v in ds):
+        TypeError('All variables of the dataset must either be sparse or dense.')
+
     logger.info('Expanding by source carrier')
-    if chunk is None:
+    if chunksize is None:
         res = ds * share
     else:
+        chunk = {'snapshot': chunksize}
         with ProgressBar():
             res = (ds.chunk(chunk) * share.chunk(chunk)).compute()
     return res.assign_attrs(ds.attrs)
@@ -56,8 +62,7 @@ def expand_by_source_type(ds, n, components=['Generator', 'StorageUnit'],
 
 
 def expand_by_sink_type(ds, n, components=['Load', 'StorageUnit'],
-                        cut_lower_share=1e-5, sparse=True,
-                        chunk=None):
+                        chunksize=None):
     """
     Breakdown allocation into demand types, e.g. Storage carriers and Load.
 
@@ -73,6 +78,9 @@ def expand_by_sink_type(ds, n, components=['Load', 'StorageUnit'],
     components : list, default ['Load', 'StorageUnit']
         List of considered components. Carrier types of these components are
         taken for breakdown.
+    chunksize : int
+        Chunksize of the snapshot chunks passed to dask for computing faster
+        and with less memory usage for large datasets.
 
     Example
     -------
@@ -84,12 +92,16 @@ def expand_by_sink_type(ds, n, components=['Load', 'StorageUnit'],
     sns = ds.get_index('snapshot')
     share = (power_demand(n, sns, per_carrier=True) / power_demand(n, sns))
     share = share.rename(bus='sink', carrier='sink_carrier')
-    if sparse:
+    if all(isinstance(ds[v].data, COO) for v in ds):
         share = as_sparse(share.fillna(0))
+    elif any(isinstance(ds[v], COO) for v in ds):
+        TypeError('All variables of the dataset must either be sparse or dense.')
+
     logger.info('Expanding by sink carrier')
-    if chunk is None:
+    if chunksize is None:
         res = ds * share
     else:
+        chunk = {'snapshot': chunksize}
         with ProgressBar():
             res = (ds.chunk(chunk) * share.chunk(chunk)).compute()
     return res.assign_attrs(ds.attrs)
